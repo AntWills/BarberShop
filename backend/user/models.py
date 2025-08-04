ee16a826bf91ac5b863f80f6b0082d185f05ae7e
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-08-04 11:18:19 -0300

    fix(backend) { user }: insufficient number of digits in the user number.

--- backend/user/models.py
+++ backend/user/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    number = models.CharField(max_length=10)
+    number = models.CharField(max_length=15)
 
     def __str__(self):
         return self.name
