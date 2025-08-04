a9c4985ca8eca253ff2532e00a3870ce4e097b58
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-08-04 10:19:23 -0300

    feat(backend) { user } : add module user.

---
+++ backend/user/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import User
+from .serializers import UserSerializer
+
+# Create your views here.
+
+
+@api_view(['GET'])
+def getData(request):
+
+    return Response("hello")
